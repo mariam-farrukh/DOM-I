cfb45f8c9f4edfe25a3d7cfff1eecc6e9ffd11a6
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-07-08 15:04:40 -0500

    Fixed bug with timer. Works Completely

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,61 +3,60 @@
 // [ ] * Has digits change to red when it gets to 10 seconds
 // [ ] * Should not count past 10 seconds
 
-const digits = document.querySelector('.digits');
-const secondTens = document.getElementById('secondTens');
-const secondOnes = document.getElementById('secondOnes');
-const msHundreds = document.getElementById('msHundreds');
-const msTens = document.getElementById('msTens');
-const msOnes = document.getElementById('msOnes');
-
+const digits = document.querySelector(".digits");
+const secondTens = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
 let msTensBegin = 0;
 let msHundredsBegin = 0;
 let secondBegin = 0;
 let tenBegin = 0;
 
-function timer(){
-    function start(){
-        let ms=0;
-        const timerInt= window.setInterval(() =>{
-            ms +=10;
-            if (ms === 10000){
-                stop(timerInt);
-            }
-            changeTime(ms);
-        }, 10);
+function timer () {
+  
+  function start () {
+    let ms = 0;
+    
+    const timerInterval = window.setInterval(() => {
+      ms += 10;
+      if (ms === 10000) {
+        stop(timerInterval); 
+      }
+      changeDigits(ms);
+    }, 10);
+  }
+
+  function changeDigits (ms) {
+    if (ms === 10000) {
+        secondTens.textContent = "1";
+        secondOnes.textContent = "0";
+        msHundreds.textContent = "0";
+        msTens.textContent = "0";
+    } else if (ms % 1000 === 0) {
+        secondBegin += 1;
+        secondOnes.textContent = secondBegin.toString();
+        msHundreds.textContent = "0";
+        msTens.textContent = "0";
+        msHundredsBegin = 0;
+        msTensBegin = 0;
+    } else if (ms % 100 === 0) {
+        msHundredsBegin += 1;
+        msHundreds.textContent = msHundredsBegin.toString();
+        msTens.textContent = "0";
+        msTensBegin = 0
+    } else {
+        msTensBegin += 1;
+        msTens.textContent = msTensBegin.toString();
     }
-    function changeTime(ms) {
-        if (ms = 10000){
-            secondTens.textContent = "1";
-            secondOnes.textContent = "0";
-            msHundreds.textContent = "0";
-            msTens.textContent = "0";
-        }
-        else if (ms % 1000 === 0){
-            secondBegin += 1;
-            secondOnes.textContent = secondBegin.toString();
-            msHundreds.textContent = "0";
-            msTens.textContent = "0";
-            msTensBegin = 0;
-            msHundredsBegin = 0;
-        }
-        else if (ms % 100 ===0) {
-            msHundredsBegin += 1;
-            msHundreds.textContent = msHundredsBegin.toString();
-            msTens.textContent = "0";
-            msTensBegin = 0;
-        }
-        else {
-            msTensBegin += 1;
-            msTens.textContent = msTensBegin.toString();
-        }
-    }
-    function stop(int){
-        clearInterval(int);
-        Array.from(digits.children).forEach(digit => {
-            digit.classList.add("redDigit");
-        })
-    }
-    start();
+  }
+
+  function stop (interval) {
+    clearInterval(interval);
+    Array.from(digits.children).forEach(digit => {
+      digit.classList.add("redDigit");
+    })
+  }
+  start();
 }
 timer();
